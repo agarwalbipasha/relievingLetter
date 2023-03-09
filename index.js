4f6ee3eddba87884a9482977942b3999d7bf6ce8
Bipasha Agarwal <agarwalbipasha@gmail.com>
2023-03-09 16:07:39 +0530

    Created variables in the html file

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const handlebars = require('handlebars');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const html = fs.readFileSync('main.html', 'utf-8');
-    await page.setContent(html, { waitUntil: 'domcontentloaded' });
+    // const html = fs.readFileSync('main.html', 'utf-8');
+    let template = handlebars.compile(fs.readFileSync('main.html', 'utf-8'));
+    let html = template({
+      date: "09 March 2023",
+      name: "ABC XYZ",
+      trainingStartDate: "01 January 2020",
+      trainingEndDate: "31 March 2020",
+      deploymentStartDate: "01 April 2020",
+      deploymentEndDate: "31 March 2023",
+      company: "MNO Pvt. Ltd"
+    })
+    await page.setContent(html, { waitUntil: 'networkidle0' });
   
     await page.emulateMediaType('screen');
   
