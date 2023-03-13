dde403ac2306c2c468fb54e0cc7736959bd14369
Sachin M K <sachinmk27@gmail.com>
2023-03-13 13:03:12 +0530

    Create only single browser instance and reuse it

--- index.js
+++ index.js
@@ -4,31 +4,29 @@ const handlebars = require('handlebars');
 const csvtojson = require('csvtojson');
 
 (async () => {
-    let template = handlebars.compile(fs.readFileSync('main.html', 'utf-8'));
+  let template = handlebars.compile(fs.readFileSync('main.html', 'utf-8'));
 
-    let cands = await csvtojson().fromFile("candidates.csv");
-      
-    cands.forEach(async(cand) => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
+  let cands = await csvtojson().fromFile('candidates.csv');
+  const browser = await puppeteer.launch();
 
-      let html = template(cand);
-      let name = cand.name;
-    
-      await page.setContent(html, { waitUntil: 'networkidle0' });
-  
-      await page.emulateMediaType('screen');
+  const promises = cands.map(async (cand) => {
+    const page = await browser.newPage();
+    let html = template(cand);
+    let name = cand.name;
 
-      const pdf = await page.pdf({
-        path: `output/${name}.pdf`,
-        margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-        printBackground: true,
-        format: 'A4',
-      });
+    await page.setContent(html, { waitUntil: 'networkidle0' });
 
-      await browser.close();
-    })
+    await page.emulateMediaType('screen');
 
-  })();
+    const pdf = await page.pdf({
+      path: `output/${name}.pdf`,
+      margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+      printBackground: true,
+      format: 'A4',
+    });
 
-
+    return pdf;
+  });
+  await Promise.all(promises);
+  await browser.close();
+})();
