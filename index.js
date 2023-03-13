85136d91665e1dc51da9ae8af2b5d94069ef22a0
Bipasha Agarwal <agarwalbipasha@gmail.com>
2023-03-13 10:58:28 +0530

    Modified the script to create individual pdf's details being taken from CSV file

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const handlebars = require('handlebars');
+const csvtojson = require('csvtojson');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // const html = fs.readFileSync('main.html', 'utf-8');
     let template = handlebars.compile(fs.readFileSync('main.html', 'utf-8'));
-    let html = template({
-      date: "09 March 2023",
-      name: "ABC XYZ",
-      trainingStartDate: "01 January 2020",
-      trainingEndDate: "31 March 2020",
-      deploymentStartDate: "01 April 2020",
-      deploymentEndDate: "31 March 2023",
-      company: "MNO Pvt. Ltd"
+
+    let cands = await csvtojson().fromFile("candidates.csv");
+      
+    cands.forEach(async(cand) => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      let html = template(cand);
+      let name = cand.name;
+    
+      await page.setContent(html, { waitUntil: 'networkidle0' });
+  
+      await page.emulateMediaType('screen');
+
+      const pdf = await page.pdf({
+        path: `output/${name}.pdf`,
+        margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+        printBackground: true,
+        format: 'A4',
+      });
+
+      await browser.close();
     })
-    await page.setContent(html, { waitUntil: 'networkidle0' });
-  
-    await page.emulateMediaType('screen');
-  
-    const pdf = await page.pdf({
-      path: 'output/result.pdf',
-      margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-      printBackground: true,
-      format: 'A4',
-    });
-  
-    await browser.close();
+
   })();
 
 
